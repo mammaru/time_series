985715b7b0e6a4ea5fb2f592e08e5c5f40d4f546
Yuma Maruyama <mauma1989@gmail.com>
2015-07-05 23:13:43 +0900

    initialize implenentation of ssm with python

--- EM.R
+++ EM.R
@@ -8,12 +8,12 @@ mu2 <- 1
 sd2 <- sqrt(0.77)
 
 if(0){
-Y <- c()
-for(i in 1:20){
+  Y <- c()
+  for(i in 1:20){
 	delta <- as.integer(runif(1,min=0,max=2))
 	y <- (1-delta)*rnorm(1,mu1,sd1) + delta*rnorm(1,mu2,sd2)
 	Y <- c(Y,y)
-}
+  }
 }
 
 Y <- c(-0.39,0.12,0.94,1.67,1.76,2.44,3.72,4.28,4.92,5.53,0.06,0.48,1.01,1.68,1.80,3.25,4.12,4.60,5.28,6.22)
@@ -34,75 +34,75 @@ count <- 0
 flag <- 0
 
 while(!flag){
-count <- count+1
+  count <- count+1
 
-#Expectation
-gamma <- rep(0,20)
-for(i in 1:20){
+  #Expectation
+  gamma <- rep(0,20)
+  for(i in 1:20){
 	tmp1 <- param_old$PI*dnorm(Y[i],param_old$MU2,param_old$SD2)
 	tmp2 <- (1-param_old$PI)*dnorm(Y[i],param_old$MU1,param_old$SD1) + param_old$PI*dnorm(Y[i],param_old$MU2,param_old$SD2)
 	gamma[i] <- tmp1/tmp2
-}
+  }
 
-#Maximization
-tmp1 <- 0
-tmp2 <- 0
-for(i in 1:20){
+  #Maximization
+  tmp1 <- 0
+  tmp2 <- 0
+  for(i in 1:20){
 	tmp1 <- tmp1 + (1-gamma[i])*Y[i]
 	tmp2 <- tmp2 + (1-gamma[i])
-}
-param_new$MU1 <- tmp1/tmp2
+  }
+  param_new$MU1 <- tmp1/tmp2
 
-tmp1 <- 0
-tmp2 <- 0
-for(i in 1:20){
+  tmp1 <- 0
+  tmp2 <- 0
+  for(i in 1:20){
 	tmp1 <- tmp1 + (1-gamma[i])*((Y[i]-param_old$MU1)^2)
 	tmp2 <- tmp2 + (1-gamma[i])	
-}
-param_new$SD1 <- tmp1/tmp2
-
-tmp1 <- 0
-tmp2 <- 0
-for(i in 1:20){
+  }
+  param_new$SD1 <- tmp1/tmp2
+  
+  tmp1 <- 0
+  tmp2 <- 0
+  for(i in 1:20){
 	tmp1 <- tmp1 + gamma[i]*Y[i]
 	tmp2 <- tmp2 + gamma[i]#sum(gamma)
-}
-param_new$MU2 <- tmp1/tmp2
-
-tmp1 <- 0
-tmp2 <- 0
-for(i in 1:20){
+  }
+  param_new$MU2 <- tmp1/tmp2
+  
+  tmp1 <- 0
+  tmp2 <- 0
+  for(i in 1:20){
 	tmp1 <- tmp1 + gamma[i]*((Y[i]-param_old$MU2)^2)
 	tmp2 <- tmp2 + gamma[i]#sum(gamma)
-}
-param_new$SD2 <- tmp1/tmp2
+  }
+  param_new$SD2 <- tmp1/tmp2
 
-tmp <- 0
-for(i in 1:20)tmp<-tmp+gamma[i]
-param_new$PI <- tmp/20
+  tmp <- 0
+  for(i in 1:20)tmp<-tmp+gamma[i]
+  param_new$PI <- tmp/20
 
-#likelihood
-tmp1 <- 0
-tmp2 <- 0
-tmp3 <- 0
-tmp4 <- 0
-for(i in 1:20){
+  #likelihood
+  tmp1 <- 0
+  tmp2 <- 0
+  tmp3 <- 0
+  tmp4 <- 0
+  for(i in 1:20){
 	tmp1 <- tmp1 + (1-gamma[i])*log(dnorm(Y[i],param_new$MU1,param_new$SD1))+gamma[i]*log(dnorm(Y[i],param_new$MU2,param_new$SD2))
 	tmp2 <- tmp2 + (1-gamma[i])*log(1-param_new$PI)+gamma[i]*log(param_new$PI)
 	#tmp3 <- tmp3 + log((1-param_new$PI)*dnorm(Y[i],param_new$MU1,param_new$SD1)+param_new$PI*dnorm(Y[i],param_new$MU2,param_new$SD2))
 	#tmp4 <- tmp4 + log((1-param_new$PI)*dnorm(Y[i],param_new$MU1,param_new$SD1)+param_new$PI*dnorm(Y[i],param_new$MU2,param_new$SD2))
-}
-llh <- c(llh,tmp1+tmp2)
-#llh <- c(llh,tmp3)
-#llh <- c(llh,tmp4)
+  }
+  llh <- c(llh,tmp1+tmp2)
+  #llh <- c(llh,tmp3)
+  #llh <- c(llh,tmp4)
 
-#check
-diff <- llh[count+1] - llh[count]
-if((diff>0&&diff<0.0001)||count==50){
+  #check
+  diff <- llh[count+1] - llh[count]
+  if((diff>0&&diff<0.0001)||count==50){
 	flag<-1
-	}else{
-		param_old <- param_new
-		}
+  }else{
+    param_old <- param_new
+  }
 
 }
 
---
+++ ssm.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+from matplotlib import pyplot as plt
+
+def myfactrial(n):
+	return 1 if n==1 else n * myfactrial(n-1)
+
+# create data
+data = DataFrame(np.random.randn(10,4),columns=["a","b","c","d"])
+sys0 = DataFrame(np.random.randn(1,4),columns=["a","b","c","d"])
+
+matF = np.matrix(np.random.randn(4,4))
+matQ = np.matrix(np.random.randn(4,4))
+matH = np.matrix(np.random.randn(4,4))
+matR = np.matrix(np.random.randn(4,4))
+
+T = 20
+
+def sys_eq(x,F,Q):
+	dim = x.shape[0]
+	return np.asarray(F * x + np.random.normal(size=(dim,1)))
+  
+def obs_eq(x,H,R):
+	dim = x.shape[0]
+	return np.asarray(H * x + np.random.normal(size=(dim,1)))
+
+sys_value = np.asarray(sys0.T)
+obs_value = np.asarray(np.zeros((4,1)))
+i = 0
+while(i < T):
+	sysi = np.matrix(sys_value)[:,i]
+	sys_value = np.hstack((sys_value, sys_eq(sysi,matF,matQ)))
+	obs_value = np.hstack((obs_value, obs_eq(sysi,matH,matR)))
+	i += 1
+
+sys_value = DataFrame(sys_value.T)
+obs_value = DataFrame(obs_value.T)
+
+
+
+class SSM:
+	def __init__(self, p, k):
+		self.sys_dim = p
+		self.obs_dim = k
+		self.x0mean = np.matrix(np.random.randn(p,1))
+		self.x0var = np.matrix(np.identity(p)) # fixed
+		self.F = np.matrix(np.random.randn(k,k))
+		self.H = np.matrix(np.eye(p,k))
+		self.Q = np.matrix(np.eye(k))
+		self.R = np.matrix(np.diag(np.random.normal(size=p)))
+
+	def test1():
+		print self.F
+		
+"""		self.xf = numeric(0)
+		self.vf = as.list(NULL)
+		self.xs0 = numeric(0)
+		self.xs = numeric(0)
+		self.vs0 = numeric(0)
+		self.vs = as.list(NULL)
+		self.vLag = as.list(NULL)
+
+	#def set_params:
+		
+	def kf(maxT, Yobs):
+
+		X0mean = self.x0mean
+		X0var = self.x0var
+		F = self.f
+		H = self.h
+		Q = self.q
+		R = self.r
+
+		x0 = np.matrix(np.random.multivariate_normal(mp.asarray(X0mean.T), np.asarray(X0var)))
+		xPri = np.matrix(sys_eq(x0,F,Q))
+		sigmaPri = [F * X0var * F.T + Q]
+
+		for(i in np.arange(maxT)){
+		  #filtering
+		  K = sigmaPri[i]*H.T*(H%*%result$sigmaPri[[i]]%*%t(H) + R).I
+		  xPost = cbind(result$xPost,result$xPri[,i] + K%*%(Yobs[,i] - H%*%result$xPri[,i]))
+		  result$sigmaPost[[i]] = result$sigmaPri[[i]] - K%*%H%*%result$sigmaPri[[i]]
+
+		  #prediction  
+		  result$xPri = cbind(result$xPri,F%*%result$xPost[,i])
+		  result$sigmaPri[[i+1]] = F%*%result$sigmaPost[[i]]%*%t(F) + Q
+
+		}
+
+		#smoothing
+		J = as.list(NULL)
+		J[[maxT]] = matrix(rep(0,k*k),k,k)
+		result$xT = matrix(result$xPost[,maxT],k,1)
+		result$sigmaT = as.list(NULL)
+		result$sigmaT[[maxT]] = result$sigmaPost[[maxT]]
+		result$sigmaLag = as.list(NULL)
+		result$sigmaLag[[maxT]] = F%*%result$sigmaPost[[maxT-1]] - K%*%H%*%result$sigmaPost[[maxT-1]]
+
+		for(i in maxT:2){
+		  J[[i-1]] = result$sigmaPost[[i-1]]%*%t(F)%*%invM(result$sigmaPri[[i]])
+		  result$xT = cbind(result$xPost[,i-1] + J[[i-1]]%*%(result$xT[,1]-result$xPri[,i]),result$xT)
+		  result$sigmaT[[i-1]] = result$sigmaPost[[i-1]] + J[[i-1]]%*%(result$sigmaT[[i]]-result$sigmaPri[[i]])%*%t(J[[i-1]])
+		}
+		for(i in maxT:3){
+		  result$sigmaLag[[i-1]] = result$sigmaPost[[i-1]]%*%t(J[[i-1]]) + J[[i-1]]%*%(result$sigmaLag[[i]]-F%*%result$sigmaPost[[i-1]])%*%t(J[[i-2]])
+		}
+		J0 = X0var%*%t(F)%*%invM(result$sigmaPri[[1]])
+		result$sigmaLag[[1]] = result$sigmaPost[[1]]%*%t(J0) + J[[1]]%*%(result$sigmaLag[[2]]-F%*%result$sigmaPost[[1]])%*%t(J0)
+		result$xT0 = X0mean + J0%*%(result$xT[,1]-result$xPri[,1])
+		result$sigmaT0 = X0var + J0%*%(result$sigmaT[[1]]-result$sigmaPri[[1]])%*%t(J0)
+
+		result$x0mean = result$xT0
+
+		return result
+
+	def likelihood():
+		return 1
+	
+	def em():
+		return 1
+"""
