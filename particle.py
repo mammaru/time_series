645ac846ae28849538987519a6b48cf1635286e7
Yuma Maruyama <mauma1989@gmail.com>
2015-07-19 10:50:57 +0900

    modify resample

--- particle.py
+++ particle.py
@@ -9,19 +9,29 @@ from timeseries import StateSpaceModel as SSM
 
 class Particle:
 	def __init__(self, d, w=0):
-		self.dim = d
-		self.position = np.random.randn(dim)
+		try:
+			self.dim = d
+		except:
+			print "Dimention of particle must be specified as augument \"d\"."
+		self.position = np.random.randn(d)
 		self.weight = w
 
 	def move(self, model):
 		self.potition = model(self.position)
-		
+
 class PF:
-	def __init__(self, num_particles=100, dim=10, model=SSM.obs_eq):
-		self.dim_particle = dim
+	def __init__(self,
+				 num_particles=100,
+				 dim_particle=10,
+				 sys_eq=SSM.sys_eq,
+				 obs_eq=SSM.obs_eq
+				 ):
+		
+		self.dim_particle = dim_particle
 		self.num_particles = num_particles
-		self.particles = [Particle(dim, 1/num_particles) for i in range(num_particles)]
-		self.prediction_func = model
+		#self.particles = [Particle(d=dim, w=1/num_particles) for i in range(num_particles)]
+		self.sys_eq = sys_eq
+		self.obs_eq = obs_eq
 	
 	def set_data(self, data):
 		self.obs = data
@@ -30,27 +40,25 @@ class PF:
 		#self.unequal_intarval_flag = True if sum(np.sum(data)) else False
 		#self.missing_data_flag = True if sum(np.sum(data)) else False
 
-	def calc_llh(self, Y):
+	def __calc_llh(self, Y):
 		# compare prediction and observation
 		for i in range(self.num_particles):
 			position = self.particles[i].position
-			weights = norm.pdf(x=position) # probability density function of normal distribution
-		#w[k][j] = gauss((z[k]-Spre[k][j]),mu2,var2)*a2; # probability density function * a2
-		#w[k][j] = gauss((z[k]-Spre[k][j]),mu2,var2)
-		#w[k][j] = w[k-1][j]*gauss((z[k]-Spre[k][j]),mu2,var2)*gauss((Spre[k][j]-Spost[k-1][j]),mu2,var2); # pdf*a2
+			weights = norm.pdf(x=position, loc=self.obs) # probability density function of normal distribution
+			#w[k][j] = gauss((z[k]-Spre[k][j]),mu2,var2)*a2; # pdf * a2
+			#w[k][j] = gauss((z[k]-Spre[k][j]),mu2,var2)
+			#w[k][j] = w[k-1][j]*gauss((z[k]-Spre[k][j]),mu2,var2)*gauss((Spre[k][j]-Spost[k-1][j]),mu2,var2); # pdf * a2
+			#t += w[k][j]; # sum of weights
+			#_Neff += w[k][j]*w[k][j]; # threashold for resample
 
-		#t += w[k][j]; # sum of weights
-		#_Neff += w[k][j]*w[k][j]; # threashold for resample
-
-	def resample(self):
-
+	def __resample(self):
+		NP = self.num_particles
 		if(Neff<NT): # Do resampling
-			c[0]=0; 
+			c = [0 for i in range(NP)]; 
 			for i in range(jmax):
-				c[i] = c[i-1] + w[k][i];
-		
+				c[i] = c[i-1] + self.particles[i].weight
 			#u[0] = ((double)random()/RAND_MAX)/(double)jmax;
-			u[0] = np.random.randn(1)
+			u[0] = np.random.randn(1)/NP
 			print "Resampling!\n1/jmax=%lf,u[%d][0]=%lf\n",1/jmax,k,u[0]
 			for j in range(jmax):
 				i = 0;
@@ -58,25 +66,20 @@ class PF:
 				while u[j]>c[i]: i += 1
 				Spost[k][j] = Spre[k][i];
 				w[k][j] = 1.0/jmax;
-
 			if k==9:
 				for j in range(jmax): print "%.20lf %.20lf\n",c[j],u[j]
-
-
 		else: # Posteriors are set to prior 
 			for j in range(jmax):
 				Spost[k][j] = Spre[k][j];
 
 
 	def execute(self):
-		PA = self.num_particles
-
-		# Scattering initial distribution
-		#double tmp_position
-		for j in range(PARTICLE_NUMBER):
-			tmp_position[0] = (360/PARTICLE_NUMBER)*j*PI/180;
-			tmp_position[1] = (360/PARTICLE_NUMBER)*j*PI/180;
-			particle[0][j].position = tmp_position; # scattering
+		DP = self.dim_particle
+		NP = self.num_particles
+		N = self.N
+		
+		# Scattering particles for initial distribution
+		self.particles = [Particle(d=DP, w=1/NP) for i in range(NP)]
 
 		# prediction and resampling		
 		for n in range(N):
@@ -90,14 +93,14 @@ class PF:
 			#}
 
 			# calculate likelihood of each particle
-			for j in range(PARTICLE_NUMBER):
-				particle[k][j].likelihood = calc_likelihood(particle[k][j].nystagmus);
+			for j in range(NP):
+				particle[k][j].likelihood = self.__calc_llh(particle[k][j].nystagmus);
 
 
-			# resampling using likelihood
+			# resample to avoid degeneracy problem
 			# decide posterior distrobution
-			for j in range(PARTICLE_NUMBER):
-				particle[k][j].position = resample(kesseki[k][j].likelihood);
+			for j in range(NP):
+				particle[k][j].position = self.__resample(kesseki[k][j].likelihood);
 
 
 
