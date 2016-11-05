6a7ee4f48e2b55d07d3c14a4f3c56445de0e3883
Yuma M <mauma1989@gmail.com>
2016-11-06 01:06:58 +0900

    Create core.py and abstract class Model.

---
+++ timeseries/model/core.py
@@ -0,0 +1,17 @@
+
+# Abstract class Model
+class Model:
+    def __init__(this):
+        pass
+        
+    def Estep(this):
+        this.expectation_delegate(this)
+
+    def Mstep(this):
+        this.maximization_delegate(this)
+
+    def expectation_delegate(this):
+        assert 0 "Not implemented expectation_delegate method in child class"
+
+    def maximization_delegate(this):
+        assert 0 "Not implemented maximization_delegate method in child class"
